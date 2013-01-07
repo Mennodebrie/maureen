c458c00d3a9e85cc325522c2668726d4b75f236a
Menno de Brie <menno@paperchip.nl>
2013-01-07 10:33:34 +0100

    initial commit

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  # config.enabled = true
+
+  # config.default_locale = :en
+
+  # config.current_locale = :en
+
+  # config.default_frontend_locale = :en
+
+  # config.frontend_locales = [:en]
+
+  # config.locales = {:ja=>"日本語", :sl=>"Slovenian", :"zh-CN"=>"Simplified Chinese", :sv=>"Svenska", :bg=>"Български", :es=>"Español", :cs=>"Česky", :"zh-TW"=>"Traditional Chinese", :pl=>"Polski", :"pt-BR"=>"Português", :en=>"English", :it=>"Italiano", :rs=>"Srpski", :nl=>"Nederlands", :fr=>"Français", :de=>"Deutsch", :da=>"Dansk", :lv=>"Latviski", :sk=>"Slovenský", :el=>"Ελληνικά", :nb=>"Norsk Bokmål", :ru=>"Русский"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaureenRedux::Application.config.secret_token = '78b21bca54c5596b77f10e26de5f9e68dc272a21d70751e83f24d69274c47b06a7d4798dbd651cfc50daef8b808903730bb85495d34dd5676bc832e23aecc651'
