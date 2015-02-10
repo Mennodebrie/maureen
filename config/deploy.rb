0c9575e0938d3aed45ccba03dcbc2b1cae6457c9
Menno de Brie <menno@paperchip.nl>
2015-02-10 13:15:11 +0100

    all

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :repository, "git@github.com:Mennodebrie/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+# ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
